068e207dae207ac732bdd637cf632d4ee8ac339c
lohith <mr.lohi007@gmail.com>
2025-06-10 23:52:28 +0530

    added backend project

---
+++ tasklyserver/src/main/java/com/lohith/tasklyserver/TasklyserverApplication.java
@@ -0,0 +1,13 @@
+package com.lohith.tasklyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklyserverApplication.class, args);
+	}
+
+}
